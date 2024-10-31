d5e525104d30105838d35ed9f095c5d5942a01d8
Tianfu Zhang <tianfu.zhang@uni-konstanz.de>
2024-10-31 15:16:56 +0100

    31/10/2024

--- launch/run.launch.py
+++ launch/run.launch.py
@@ -1,13 +1,37 @@
 import os
 from launch import LaunchDescription
+from launch.actions import SetEnvironmentVariable
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='information_dissemination',  
-            executable='information_dissemination',  
-            name='information_dissemination',  
-            output='screen',  
-        ),
-    ])
+    # Set environment variable for QT_QPA_PLATFORM
+    # This line ensures that rviz uses the correct platform
+    set_qt_platform = SetEnvironmentVariable('QT_QPA_PLATFORM', 'xcb')
+
+    nodes = []
+
+    for i in range(20): 
+        nodes.append(
+            Node(
+                package='information_dissemination',  
+                executable='controller',  
+                name=f'controller',  
+                namespace=f'bot{i}',  
+                output='screen',  
+            )
+        )
+ 
+    rviz_node = Node(
+        package='rviz2',  
+        executable='rviz2',  
+        name='rviz',  
+        output='screen',  
+        arguments=['-d', "/home/tianfu/information_dissemination_ws/src/information_dissemination/launch/rviz/defaul.rviz"],  
+    )
+    
+
+    nodes.append(rviz_node)
+
+  
+    return LaunchDescription([set_qt_platform] + nodes)
+
--- src/controller.h
+++ src/controller.h
@@ -1,19 +1,68 @@
+#include <cmath>                               
+#include <angles/angles.h>   
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/vector3.hpp"
-#include "geometry_msgs/msg/quaternion.hpp"
-#include "information_dissemination/msg/position.h"
+#include "nav_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include <tf2/utils.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp> 
+       
+
 
 class Controller : public rclcpp::Node
 {
 public:
     Controller() : Node("controller")
     {
-       
+        pose_subscription_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+            "position", 10, std::bind(&Controller::pose_callback, this, std::placeholders::_1));
+        cmd_vel_subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
+            "cmd_vel", 10, std::bind(&Controller::cmd_vel_callback, this, std::placeholders::_1));
+
+        path_publisher_ = this->create_publisher<nav_msgs::msg::Path>("path_history", 10);
+        predict_path_publisher_ = this->create_publisher<nav_msgs::msg::Path>("path_predict", 10);
+
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(500), std::bind(&Controller::ControlStep, this));
+
     }
 
 private:
+    geometry_msgs::msg::PoseStamped current_pose;
+    geometry_msgs::msg::Twist current_vel;
+    nav_msgs::msg::Path path_msg;
+    nav_msgs::msg::Path path_predict;
 
+    void pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+    {
+        current_pose = *msg;
+        current_pose.header.frame_id = "map";
+    }
 
+    void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
+    {
+        current_vel = *msg;
+    }
+
+    void ControlStep()
+    {
+        publish_path();
+        path_pridect();
+    };
+
+    void publish_path();
+
+    void path_pridect();
+
+    void local_communication();
+
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_subscription_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_subscription_;
+
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr predict_path_publisher_;
+
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char *argv[])
